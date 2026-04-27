b03ee4ef311866e98caeb0e0e0550137abf4d8dd
OGDichter <dertom.hans2023@gmail.com>
2026-04-27 10:23:44 +0200

    fixes-db

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,7 +2,11 @@
 import { Pool } from 'pg';
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString:
+    process.env.DATABASE_URL ||
+    process.env.POSTGRES_URL ||
+    process.env.POSTGRES_URL_NON_POOLING ||
+    process.env.SUPABASE_DATABASE_URL,
 });
 
 export const initializeDatabase = async () => {
